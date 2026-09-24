91df53de62b159efbe0816ad2b760e4fe6e53d50
TIGER <155530540+dottoenock@users.noreply.github.com>
2026-09-24 21:36:23 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,30 +1,9 @@
-require('dotenv').config();
-
-global.APIs = {
-    xteam: 'https://api.xteam.xyz',
-    dzx: 'https://api.dhamzxploit.my.id',
-    lol: 'https://api.lolhuman.xyz',
-    violetics: 'https://violetics.pw',
-    neoxr: 'https://api.neoxr.my.id',
-    zenzapis: 'https://zenzapis.xyz',
-    akuari: 'https://api.akuari.my.id',
-    akuari2: 'https://apimu.my.id',
-    nrtm: 'https://fg-nrtm.ddns.net',
-    bg: 'http://bochil.ddns.net',
-    fgmods: 'https://api-fgmods.ddns.net'
-};
-
-global.APIKeys = {
-    'https://api.xteam.xyz': 'd90a9e986e18778b',
-    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-    'https://api.neoxr.my.id': 'yourkey',
-    'https://violetics.pw': 'beta',
-    'https://zenzapis.xyz': 'yourkey',
-    'https://api-fgmods.ddns.net': 'fg-dylux'
-};
-
 module.exports = {
-    WARN_COUNT: 3,
-    APIs: global.APIs,
-    APIKeys: global.APIKeys
+    BOT_NAME: "𝑻𝑰𝑮𝑬𝑹-𝑴𝑫",
+    OWNER_NAME: "Tiger",
+    OWNER_NUMBER: "255692788410", //
+    PREFIX: ".",
+    AUTO_READ_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    STATUS_EMOJI: "💚"
 };
